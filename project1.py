d906b2ccefa318bbd6a3847e3db5722f1c5b5baa
ashraymallesh <ashraymallesh23@gmail.com>
2019-09-18 16:31:53 -0400

    create a new column for binary good/bad labels

---
+++ project1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+# Import data into pandas dataframe
+winedf = pd.read_csv("winequality-red.csv") #wine dataframe
+
+"""
+# Check for null data
+null_data = winedf[winedf.isnull().any(axis=1)]
+print(null_data)
+"""
+
+def label_binary_quality(row):
+    if row["quality"] <= 0.5:
+        return 0
+    else:
+        return 1
+winedf["binary_quality"] = winedf.apply(lambda row: label_binary_quality(row), axis=1)
