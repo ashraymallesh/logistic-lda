a428f2188f946c5218db8d822d08a8efd015a297
ashraymallesh <ashraymallesh23@gmail.com>
2019-09-27 01:34:09 -0400

    add LDA class and refactor some variables

--- test.py
+++ test.py
@@ -1,8 +1,2 @@
 import pandas as pd
 import numpy as np
-
-#Data Cleaning
-winedf = pd.read_csv("winequality-red.csv", sep=';')
-wineData = winedf.to_numpy()
-wineData[:,-1] = (wineData[:,-1]>=6).astype(int) #convert 6+ to 1 and <5 to 0
-y = wineData[:,-1][:, np.newaxis]
